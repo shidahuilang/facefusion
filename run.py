ecb467aef29175f5e05e042f0c5e6d4c0342602c
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2024-06-19 21:32:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 from facefusion import core
 from rich.console import Console
 from rich.progress import track
@@ -8,6 +9,7 @@ import time
 console = Console()
 
 def display_startup_info():
+    os.system('title 大灰狼-FaceFusion 启动脚本')  # 设置控制台标题
     console.print("[bold yellow]大灰狼-FaceFusion[/bold yellow]")
     console.print("[bold yellow]版本: v2.6.1[/bold yellow]\n")
     console.print("[bold green]启动中...请稍等[/bold green]\n")
@@ -17,4 +19,3 @@ def display_startup_info():
 if __name__ == '__main__':
     display_startup_info()
     core.cli()
-    console.print("\n[bold cyan]FaceFusion 处理完成![/bold cyan]")
